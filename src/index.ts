4cb9bdaf190a8d148b62e251219251a083c7f0fa
Trisna Utama <trisnautama@gmail.com>
2025-05-27 20:17:16 +0700

    init:ci/cd pipeline

--- src/index.ts
+++ src/index.ts
@@ -46,5 +46,5 @@ app
 	)
 	.listen({ port: 8000, hostname: "0.0.0.0" });
 console.log(
-	`🦊 🦊 🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port} 🦊 🦊 🦊`,
+	`🦊 🦊 🦊 🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port} 🦊 🦊 🦊 🦊`,
 );
