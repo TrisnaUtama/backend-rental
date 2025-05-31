1bb33915e36332a017f138a4520b287f1f63c81c
Trisna Utama <trisnautama@gmail.com>
2025-05-31 15:39:42 +0700

    feat: adding relation to travel_pax and making repository, service and updating route in travek_package

---
+++ src/infrastructure/repositories/travelPax.repo.ts
@@ -0,0 +1,62 @@
+import "reflect-metadata";
+import { injectable, inject } from "inversify";
+import { type CreatePax, TYPES, type UpdatePax } from "./../entity/types";
+import type { ErrorHandler } from "../entity/errors/global.error";
+import type { Prisma, PrismaClient } from "@prisma/client";
+import type { ITravelPax } from "../entity/interfaces";
+
+@injectable()
+export class TravelPaxRepository implements ITravelPax {
+  private errorHandler: ErrorHandler;
+  private prisma: PrismaClient;
+
+  constructor(
+    @inject(TYPES.errorHandler) errorHandler: ErrorHandler,
+    @inject(TYPES.prisma) prisma: PrismaClient
+  ) {
+    this.errorHandler = errorHandler;
+    this.prisma = prisma;
+  }
+
+  async getAll() {
+    try {
+      return await this.prisma.travel_Packages_Pax.findMany();
+    } catch (error) {
+      this.errorHandler.handleRepositoryError(error);
+    }
+  }
+
+  async getOne(id: string) {
+    try {
+      return await this.prisma.travel_Packages_Pax.findUnique({
+        where: { id },
+      });
+    } catch (error) {
+      this.errorHandler.handleRepositoryError(error);
+    }
+  }
+
+  async create(payload: CreatePax[], tx?: Prisma.TransactionClient) {
+    try {
+      const client = tx || this.prisma;
+      return await client.travel_Packages_Pax.createMany({ data: payload });
+    } catch (error) {
+      this.errorHandler.handleRepositoryError(error);
+    }
+  }
+
+  async update(payload: UpdatePax[], tx?: Prisma.TransactionClient) {
+    try {
+      const client = tx || this.prisma;
+      const updatePromises = payload.map((travelPax) =>
+        client.travel_Packages_Pax.update({
+          where: { id: travelPax.id },
+          data: travelPax,
+        })
+      );
+      return await Promise.all(updatePromises);
+    } catch (error) {
+      this.errorHandler.handleRepositoryError(error);
+    }
+  }
+}
