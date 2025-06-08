01c584c2d69a63cafb9910d7981ac7211d37f486
Trisna Utama <trisnautama@gmail.com>
2025-06-08 22:05:48 +0700

    init

--- src/infrastructure/repositories/travelPack.repo.ts
+++ src/infrastructure/repositories/travelPack.repo.ts
@@ -69,6 +69,9 @@ export class TravelPackageRepository implements ITravelPackages {
 						where: {
 							deleted_at: null,
 						},
+						include: {
+							destination: true
+						}
 					},
 				},
 			});
